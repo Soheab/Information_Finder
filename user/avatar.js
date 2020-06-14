cfa2f5b01f806cd1f49f6d606ad5f83c67196eea
ghostlydilemma <ghostly.dilemma@gmail.com>
2020-06-14 16:14:50 +0200

    Fix User Avatar Command

--- user/avatar.js
+++ user/avatar.js
@@ -3,10 +3,9 @@ module.exports = {
   description: "Get your avatar URL",
   func: (ctx) => {
     const user = ctx.getCurrentUser()
-    const user_avatar_type = (user.icon.startsWith("a_")) ? "gif" : "png"
     return ctx.send_message(
       "Your avatar URL is " +
-      `https://cdn.discordapp.com/avatars/${user.id}/${user.icon}.${server_icon_type}?size=1024`
+      user.avatarURL
     )
   }
 };
